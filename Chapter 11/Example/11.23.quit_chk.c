01c29b8fa0215580aaa459df4a45330de56857d2
BiaoChan <1223030325@qq.com>
2020-03-25 22:48:50 +0800

    update

--- Chapter 11/Example/11.23.quit_chk.c
+++ Chapter 11/Example/11.23.quit_chk.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE
+#define SIZE 80
+#define LIM 10
+#define STOP "quit"
 
 char * s_gets(char *st, int n);
 
+int main(void)
+{
+    char input[LIM][SIZE];
+    int ct=0;
 
+    printf("Enter up to %d lines (type quit to quit):\n", LIM);
+    while(ct<LIM && s_gets(input[ct], SIZE) && strcmp(input[ct], STOP))
+        ct++;
+    printf("%d strings entered.\n", ct);
+    
+    return 0;
+}
 
 
 
